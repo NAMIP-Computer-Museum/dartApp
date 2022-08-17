a40830d1607de4988219e04dc4a41879bda7d93b
Zilba26 <basile.lebaudy@gmail.com>
2022-08-17 11:03:12 +0200

    Ajout données Images et Videos des composants

--- lib/db_helper.dart
+++ lib/db_helper.dart
@@ -1,1 +1,21 @@
-//class DBHelper
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DBHelper {
+
+  Future<Database> database() async {
+    final dbPath = await getDatabasesPath();
+    return await openDatabase(
+      join(dbPath, ''),
+    );
+  }
+
+  static late Database _db;
+
+  Future<Database> get db async {
+    // https://camposha.info/flutter/fr/flutter-sqflite/#gsc.tab=0
+    if (_db != null) return _db;
+    _db = await database();
+    return _db;
+  }
+}
